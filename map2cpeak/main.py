6e42be4e4ee6b2d333eee0d443739c83f79c3d87
Xinze Wu <wxz22@mails.tsinghua.edu.cn>
2023-09-06 14:26:16 +0800

    Update main.py

--- map2cpeak/main.py
+++ map2cpeak/main.py
@@ -1,31 +1,28 @@
 # python 
-# 2023/08/14
-# version 1.2
-# @auther : Xinze Wu
+# 2023-09-05
+# version 2.0
+# @auther : Xinze Wus
 
 import os
 import sys
 import gzip
-from tqdm import tqdm
-from scipy.io import mmread
-from collections import OrderedDict
-from joblib import Parallel, delayed
-import numpy as np
-import scanpy as sc
 import anndata
 import argparse
 import pandas as pd
+import numpy as np
+import scanpy as sc
+from tqdm import tqdm
+from scipy.io import mmread
+from collections import Counter,OrderedDict
+
 
 
 # map a bed to b bed, the output is a tuple of two numpy arrays: (indices, values)
 def map_A_to_B(a_peaks):
     
-    chromosomes = list(range(1, 23))
-    chromosomes.extend(["X", "Y"])
-    chrlist = ["chr" + str(chrom) for chrom in chromosomes]
     
     a_dict = {}
-    for chrom in chrlist:
+    for chrom in chr_list:
         a_dict[chrom] = []
     
     for peak in a_peaks:
@@ -54,63 +51,93 @@ def map_A_to_B(a_peaks):
 
 
 # def function to trans fragment file to cpeaks referenced mtx
-def frag2mtx(fragment_path,savepath,barcode_path,num_cores):
+def frag2mtx(fragment_path,savepath,barcode_path):
     '''
     fragment_path: path of fragment.gz file
     savepath: path to save mtx file
     barcode_path: path of barcode file, ,txt format,each line is a barcode; if None, use all barcode in fragment.gz file
     '''
+    bar2idx = OrderedDict()
     
-    # get unique barcode
-
-    barcodes = open(barcode_path, 'r').readlines()
-    barcodes = [i.strip() for i in barcodes]
-    print('num of barcode is {}'.format(len(barcodes)))
+    if barcode_path == None:
 
 
-    # get the bed in each barcode
-    print('start to get bed in each barcode')
+        # 打开压缩文件并且逐行读取
+        with gzip.open(fragment_path, 'rt') as file:
+            for line in tqdm(file):
+                tmp = line.strip().split('\t')
+                if len(tmp) < 5 or tmp[0] not in chr_list:
+                    continue
+                if tmp[3] not in bar2idx:
+                    bar2idx[tmp[3]] = []
 
-    fragment_data = gzip.open(fragment_path, 'rb')
-    
-    bar2bed = OrderedDict()
-    for barcode in barcodes:
-        bar2bed[barcode] = []
-    
-    for line in tqdm(fragment_data):
-        temp = line.decode().strip().split('\t')
-        try:
-            bar2bed[temp[3]].append((temp[0],int(temp[1]),int(temp[2])))
-        except:
-            continue
-    print('num peak of random bar: ',len(bar2bed[barcodes[-1]]))
-    print('num peak of random bar: ',len(bar2bed[barcodes[-2]]))
-    print('num peak of random bar: ',len(bar2bed[barcodes[-3]]))
+                idx_set = set()
+                for i in range(int(tmp[1]), int(tmp[2]), 59):
+                    if i in dic_chr[tmp[0]].keys():
+                        idx_set.add(dic_chr[tmp[0]][i])
+
+                if int(tmp[2])-1 in dic_chr[tmp[0]]:
+                    idx_set.add(dic_chr[tmp[0]][int(tmp[2])-1])
+                bar2idx[tmp[3]] += list(idx_set)
+
+        value_num = 0
+        bar2dic = OrderedDict()
+        for i,j in bar2idx.items():
+            counter = Counter(j)
+            bar2dic[i] = counter
+            value_num+=len(counter)
+
+        fb = open(os.path.join(savepath,'barcodes.txt'),'w')
+        fm = open(os.path.join(savepath,output_name+'.mtx'),'w')
 
 
-    print('mapping to cpeaks...')
+        fm.write('%%MatrixMarket matrix coordinate real general\n')
+        fm.write(f'{len(b_peaks)} {len(bar2idx)} {value_num}\n')
 
-    results = Parallel(n_jobs=num_cores, backend="multiprocessing")(
-        delayed(map_A_to_B)(input_list) for input_list in tqdm(list(bar2bed.values()))
-    )
-    
-    value_num = 0
-    for res in results:
-        value_num += len(res[0])
-    
-    print('the num of the value of mtx is: ',value_num)
-    
-    print('start to write mtx file')
-    global mtx_path
-    mtx_path= os.path.join(savepath,output_name+'.mtx')
-    
-    with open(mtx_path, 'w') as f:
-        f.write('%%MatrixMarket matrix coordinate real general\n')
-        f.write(f'{len(b_peaks)} {len(barcodes)} {value_num}\n')
-        
-        for i,res in enumerate(results):
-            for j in range(len(res[0])):
-                f.write(f'{res[0][j]} {i+1} {res[1][j]}\n')
+        for idx,(i,dic) in enumerate(bar2dic.items()):
+            fb.write(i+'\n')
+            for c,v in dic.items():
+                fm.write(f'{c+1} {idx+1} {v}\n')
+
+        fb.close()
+        fm.close()
+    else:
+        barcodes = [i.strip() for i in open(barcode_path).readlines()]
+        for barcode in barcodes:
+            bar2idx[barcode] = []
+            
+        with gzip.open(fragment_path, 'rt') as file:
+            for line in tqdm(file):
+                tmp = line.strip().split('\t')
+                if len(tmp) < 5 or tmp[0] not in chr_list or tmp[3] not in bar2idx:
+                    continue
+
+                idx_set = set()
+                for i in range(int(tmp[1]), int(tmp[2]), 59):
+                    if i in dic_chr[tmp[0]].keys():
+                        idx_set.add(dic_chr[tmp[0]][i])
+
+                if int(tmp[2])-1 in dic_chr[tmp[0]]:
+                    idx_set.add(dic_chr[tmp[0]][int(tmp[2])-1])
+                bar2idx[tmp[3]] += list(idx_set)
+        value_num = 0
+        bar2dic = OrderedDict()
+        for i,j in bar2idx.items():
+            counter = Counter(j)
+            bar2dic[i] = counter
+            value_num+=len(counter)
+
+        fm = open(os.path.join(savepath,output_name+'.mtx'),'w')
+
+        fm.write('%%MatrixMarket matrix coordinate real general\n')
+        fm.write(f'{len(b_peaks)} {len(bar2idx)} {value_num}\n')
+
+        for idx,(i,dic) in enumerate(bar2dic.items()):
+            for c,v in dic.items():
+                fm.write(f'{c+1} {idx+1} {v}\n')
+
+        fm.close()
+            
     print('fragment to mtx done!')
 
 
@@ -148,12 +175,9 @@ def map_bed_to_bed(a_bed_path, b_peaks,savepath):
     a_peaks = open(a_bed_path, 'r').readlines()
     a_peaks = [i.strip().split('\t') for i in a_peaks]
 
-    chromosomes = list(range(1, 23))
-    chromosomes.extend(["X", "Y"])
-    chrlist = ["chr" + str(chrom) for chrom in chromosomes]
     
     a_dict = {}
-    for chrom in chrlist:
+    for chrom in chr_list:
         a_dict[chrom] = []
     
     for peak in a_peaks:
@@ -190,78 +214,75 @@ if __name__ == "__main__":
     parser.add_argument("--output", '-o',type=str, default='./map2cpeaks_result',help="path to save files")
     parser.add_argument("--output_name",type=str, default='cell-cpeaks',help="name of output files e.g. cell-cpeaks")
     parser.add_argument("--type_saved", '-t',type=str, default='mtx',help="save type, h5ad or mtx")
-    parser.add_argument("--num_cores",'-n', type=int, default= 10,help="num of cores to use")
     parser.add_argument("--bed_path", '-bed',type=str, default=None,help="bed file you called before")
     parser.add_argument("--reference",type=str, default='hg38',help="cpeaks version: hg38 or hg19")
 
     args = parser.parse_args()
+    
    
     from time import time
     time_ = time()
 
-    fragment_path = args.fragment_pathll
+    fragment_path = args.fragment_path
     barcode_path = args.barcode_path
     savepath = args.output
     save_type = args.type_saved
-    num_cores = args.num_cores
     bed_path = args.bed_path
     reference = args.reference
     output_name = args.output_name
+    
+    # fragment_path not end with 'tsv.gz' and not None
+    if fragment_path == None or fragment_path[-6:] != 'tsv.gz':
+        raise ValueError('fragment_path does not end with tsv.gz')
+    
+    if barcode_path is None:
+        
+        print("using all barcodes in the fragment file")
+        
+    
+    chr_list = ['chr'+str(i) for i in range(1,23)]+['chrX','chrY']
+
 
     if reference == 'hg38':
-        cpeaks_path = 'cpeaks_hg38.bed'
+        cpeaks_path = 'cpeaks_hg38.bed.gz'
     else:
-        cpeaks_path = 'cpeaks_hg19.bed'
+        cpeaks_path = 'cpeaks_hg19.bed.gz'
 
     # get cpeaks
+    dic_chr = OrderedDict()
+    b_peaks = []
+    for i in range(1,23):
+        dic_chr[f'chr{i}'] = OrderedDict()
+    dic_chr['chrX'] = OrderedDict()
+    dic_chr['chrY'] = OrderedDict()
+    
     try:
-        print('reading cpeaks')
-        with open(cpeaks_path) as b_file:
+        print("read and load cpeaks... 3 min is need...",end = ' ')
+        with gzip.open(cpeaks_path,'rt') as b_file:
             b_lines = b_file.readlines()
-        b_peaks = []
-        for line in tqdm(b_lines):
             
+        for idx,line in enumerate(b_lines):
             fields = line.strip().split('\t')
-            b_peaks.append((fields[0], int(fields[1]), int(fields[2])))  
+            b_peaks.append((fields[0], int(fields[1]), int(fields[2]))) 
+            for j in range(int(fields[1]),int(fields[2])):
+                dic_chr[fields[0]][j] = idx
+        print('done')
+             
     except:
-        raise('there is sth wrong with cpeaks file')
+        raise('there is sth wrong with cpeaks file, plz send email to wxz22@mails.tsinghua.edu.cn')
     
     if not os.path.exists(savepath):
         os.makedirs(savepath)
 
-
     if bed_path is not None:
         print('attention: you use bed file you called before')
         map_bed_to_bed(bed_path, b_peaks,os.path.join(savepath,'res.bed'))
-        # 退出
+        print("finished!")
         sys.exit(0)
 
-    # fragment_path not end with 'tsv.gz' and not None
-    if fragment_path[-6:] != 'tsv.gz':
-        print(fragment_path[-6:])
-        raise('fragment_path not end with tsv.gz')
     
-    if barcode_path is None:
-        
-        barcode_path = os.path.join(savepath,'barcodes.txt')
-       
-        print('start to get barcode')
-        fragment_data = gzip.open(fragment_path, 'rb')
-
-        barcodes = set()
-        for line in tqdm(fragment_data):
-            line = line.decode()
-            barcodes.add(line.split('\t')[3])
-        print('save {} barcodes to {}'.format(len(barcodes),barcode_path))
-        # write barcode to file
-        
-        with open(os.path.join(savepath,'barcodes.txt'), 'w') as f:
-            for barcode in barcodes:
-                f.write(barcode+'\n')
-
-
     if save_type == 'mtx':
-        frag2mtx(fragment_path,savepath,barcode_path,num_cores)
+        frag2mtx(fragment_path,savepath,barcode_path)
     else:
         
         frag2mtx(fragment_path,savepath,barcode_path,num_cores)
